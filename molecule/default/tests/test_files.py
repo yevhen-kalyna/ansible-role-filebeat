f4e87c892a4bc0ae2a8c0a3b1d7a4cee622927f8
Yevhen Kalyna <e.kalina.work@gmail.com>
2019-09-18 19:06:05 +0300

    Added test, filters Fixed installing in deb Added service state check

---
+++ molecule/default/tests/test_files.py
@@ -0,0 +1,37 @@
+"""Test for files."""
+import os
+import testinfra.utils.ansible_runner
+import pytest
+
+testinfra_hosts = testinfra.utils.ansible_runner.AnsibleRunner(
+    os.environ['MOLECULE_INVENTORY_FILE']).get_hosts('all')
+
+
+@pytest.mark.parametrize("name, content", [
+    ('/etc/yum.repos.d/elasticsearch-6.x.repo',
+     'baseurl = https://artifacts.elastic.co/packages/6.x/yum'),
+    ('/etc/filebeat/filebeat.yml',
+     'filebeat.config.modules:'),
+    ('/etc/filebeat/filebeat.yml',
+     'filebeat.prospectors:'),
+    ('/etc/filebeat/filebeat.yml',
+     'output.elasticsearch:'),
+    ('/etc/filebeat/filebeat.yml',
+     'setup.template.settings:'),
+    ('/etc/filebeat/modules.d/system.yml',
+     'syslog:'),
+    ('/etc/filebeat/modules.d/system.yml',
+     '- taga'),
+    ('/etc/filebeat/modules.d/system.yml',
+     'var.paths: \["/var/log/messages", "/var/log/secure"\]'),
+    ('/etc/filebeat/filebeat.yml',
+     'system: true'),
+    ('/etc/filebeat/filebeat.yml',
+     'setup.kibana.host: "localhost:5601"'),
+])
+def test_files(host, name, content):
+    """Test that file exists."""
+    f = host.file(name)
+    assert f.exists
+    assert f.is_file
+    assert f.contains(content)
